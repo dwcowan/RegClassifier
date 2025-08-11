45f98a971d4b6f62d8761f74918b541be5183da3
dwcowan <83236007+dwcowan@users.noreply.github.com>
2025-08-11 18:01:16 +0100

    sanity

--- +reg/ensure_db.m
+++ +reg/ensure_db.m
@@ -6,7 +6,8 @@ if isfield(DB,'vendor') && strcmpi(DB.vendor,'sqlite')
     if isfile(DB.sqlite_path)
         sconn = sqlite(DB.sqlite_path);          % open existing file
     else
-<<<<<<< HEAD
+
+        <<<<<<< HEAD
         sconn = sqlite(DB.sqlite_path,'create'); % create new file
 =======
         sconn = sqlite(DB.sqlite_path, 'create'); %#ok<SQLITE> % create new file
